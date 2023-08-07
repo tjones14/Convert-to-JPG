74454e248422d67dd63fe6317ed060aff2f8e45c
Tim Jones <tazarian14@gmail.com>
2023-08-07 10:39:29 -0400

    Add project files.

---
+++ Convert to JPG/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace HEICtoJPG
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dropPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioJPG = new System.Windows.Forms.RadioButton();
+            this.radioPDF = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dropPanel
+            // 
+            this.dropPanel.AllowDrop = true;
+            this.dropPanel.AutoSize = true;
+            this.dropPanel.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.dropPanel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dropPanel.Location = new System.Drawing.Point(12, 43);
+            this.dropPanel.Name = "dropPanel";
+            this.dropPanel.Size = new System.Drawing.Size(623, 340);
+            this.dropPanel.TabIndex = 0;
+            this.dropPanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.DropPanel_DragDrop);
+            this.dropPanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.DropPanel_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Leelawadee", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(647, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drag and Drop images to be converted";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Font = new System.Drawing.Font("Leelawadee", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(504, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Process Images";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Enabled = false;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 434);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(327, 49);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Processing Images";
+            // 
+            // radioJPG
+            // 
+            this.radioJPG.AutoSize = true;
+            this.radioJPG.Location = new System.Drawing.Point(21, 14);
+            this.radioJPG.Name = "radioJPG";
+            this.radioJPG.Size = new System.Drawing.Size(44, 19);
+            this.radioJPG.TabIndex = 4;
+            this.radioJPG.TabStop = true;
+            this.radioJPG.Text = "JPG";
+            this.radioJPG.UseVisualStyleBackColor = true;
+            // 
+            // radioPDF
+            // 
+            this.radioPDF.AutoSize = true;
+            this.radioPDF.Location = new System.Drawing.Point(71, 14);
+            this.radioPDF.Name = "radioPDF";
+            this.radioPDF.Size = new System.Drawing.Size(46, 19);
+            this.radioPDF.TabIndex = 5;
+            this.radioPDF.TabStop = true;
+            this.radioPDF.Text = "PDF";
+            this.radioPDF.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioPDF);
+            this.groupBox1.Controls.Add(this.radioJPG);
+            this.groupBox1.Location = new System.Drawing.Point(518, 396);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(128, 45);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(445, 412);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Convert To:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 495);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dropPanel);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Convert to Jpg";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel dropPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioJPG;
+        private System.Windows.Forms.RadioButton radioPDF;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Convert to JPG/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using ImageMagick;
+using System.IO;
+using System.Windows.Forms;
+using System.Text;
+using Spire.Doc;
+
+namespace HEICtoJPG
+{
+    public partial class Form1 : Form
+    {
+        public List<string> fileList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fileList = new List<string>();
+        }
+
+        private void DropPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e != null)
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void DropPanel_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e != null)
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string file in files)
+                {
+                    fileList.Add(file);
+                }
+            }
+            listBox1.Items.Clear();
+            foreach (var item in fileList)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (radioJPG.Checked == true)
+            {
+                foreach (var file in fileList)
+                {
+                    if (File.Exists(file))
+                    {
+                        var fileName = Path.GetFileName(file);
+                        var path = Path.GetDirectoryName(file);
+                        path = path + @"\out-";
+
+                        using (MagickImage image = new MagickImage(file))
+                        {
+                            string newfile = file.Replace(Path.GetExtension(file), ".jpg");
+                            var newFileName = Path.GetFileName(newfile);
+                            var outFile = path + newFileName;
+                            image.Write(outFile);
+                        }
+                    }
+
+                }
+            }
+
+            if (radioPDF.Checked == true)
+            {
+                foreach (var file in fileList)
+                {
+                    if (File.Exists(file))
+                    {
+                        var fileName = Path.GetFileName(file);
+                        var path = Path.GetDirectoryName(file);
+                        //path = path + @"\out-";
+
+                        Document doc = new Document();
+                        doc.LoadFromFile(file);
+
+                        string newPdfFile = file.Replace(Path.GetExtension(file), ".pdf");
+                        var newPdfFileName = Path.GetFileName(newPdfFile);
+                        var outfile = path + @"\" + newPdfFileName;
+
+                        doc.SaveToFile(outfile, FileFormat.PDF);
+
+                    }
+
+                }
+            }
+
+            if (radioJPG.Checked==true || radioPDF.Checked == true)
+            {
+                listBox1.Items.Clear();
+                fileList.Clear();
+                MessageBox.Show("All Files Processed");
+            }
+            else
+            {
+                MessageBox.Show("Please select a type of conversion.");
+            }
+        }
+    }
+}
